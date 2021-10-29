5be60cda0eac25faab0defdd43cf88985d0e1059
Meheedi-Hasan <mahdyhassann@gmail.com>
2021-10-29 19:30:31 +0600

    showing some errors in the subTask, list and shoppingList  methods

--- src/test/java/tests/CreateTask.java
+++ src/test/java/tests/CreateTask.java
@@ -135,11 +135,9 @@ public class CreateTask {
     	timeout.timeout();
     	commonMethods.setTextWithClick(Locators.subTaskItemBtn, Locators.subTaskItemText);
     	timeout.timeout();
-    	commonMethods.clickOnButton(Locators.justClick);
-    	timeout.timeout();
+    	//commonMethods.clickOnButton(Locators.justClick);
+    	//timeout.timeout();
     	Thread.sleep(2000);
-        //String subTask = commonMethods.getText(Locators.wakeUpEarly);
-       // assertEquals(subTask, Locators.subTaskItemText);
         System.out.println("Successfully sub task created");
     }
     @Test(priority = 4)
@@ -152,93 +150,24 @@ public class CreateTask {
     	timeout.timeout();
     	commonMethods.clickOnButton(Locators.listBtn);
     	timeout.timeout();
-//        String list = commonMethods.getText(Locators);
-//        assertEquals(list, Locators);
         System.out.println("Successfully New list appended on list category");
     }
     
-    
-    
-       
-//    //sneakers
-//    @Test(priority = 1)
-//    public void sneakersTest () {
-//        timeout.timeout();
-//        commonMethods.clickOnButton(Locators.sneakers);
-//        timeout.timeout();
-//
-//        assertEquals(homePage.getCurrentUrl(), Locators.sneakersUrl);
-//        System.out.println("Successfully visit sneakers");
-//    }
-
-//    //koreanBeauty
-//    @Test (priority = 2)
-//    public void koreanBeauty () {
-//        commonMethods.back();
-//        timeout.timeout();
-//        commonMethods.clickOnButton(Locators.koreanBeauty);
-//        timeout.timeout();
-//
-//        assertEquals(homePage.getCurrentUrl(), Locators.koreanBeautyUrl);
-//        System.out.println("Successfully visit koreanBeauty");
-//    }
-//
-//    //wristWatches
-//    @Test (priority = 3)
-//    public void wristWatches () {
-//        commonMethods.back();
-//        timeout.timeout();
-//        commonMethods.clickOnButton(Locators.wristWatches);
-//        timeout.timeout();
-//
-//        assertEquals(homePage.getCurrentUrl(), Locators.wristWatchesUrl);
-//        System.out.println("Successfully visit wristWatches");
-//    }
-//    //fishing
-//    @Test (priority = 4)
-//    public void fishing () {
-//        commonMethods.back();
-//        timeout.timeout();
-//        commonMethods.clickOnButton(Locators.fishing);
-//        timeout.timeout();
-//
-//        assertEquals(homePage.getCurrentUrl(), Locators.fishingUrl);
-//        System.out.println("Successfully visit fishing");
-//    }
-//    //collectibles
-//    @Test (priority = 5)
-//    public void collectibles () {
-//        commonMethods.back();
-//        timeout.timeout();
-//        commonMethods.clickOnButton(Locators.collectibles);
-//        timeout.timeout();
-//
-//        assertEquals(homePage.getCurrentUrl(), Locators.collectiblesUrl);
-//        System.out.println("Successfully visit collectibles");
-//    }
-//    //smartPhones
-//    @Test (priority = 6)
-//    public void healthAndBeautyTest () {
-//        commonMethods.back();
-//        timeout.timeout();
-//        commonMethods.clickOnButton(Locators.smartPhones);
-//        timeout.timeout();
-//
-//        assertEquals(homePage.getCurrentUrl(), Locators.smartPhonesUrl);
-//        System.out.println("Successfully visit smartPhones");
-//    }
-//
-//    //popularSell
-//    @Test (priority = 7)
-//    public void homeAndGardenTest () {
-//        commonMethods.back();
-//        timeout.timeout();
-//        commonMethods.clickOnButton(Locators.popularSell);
-//        timeout.timeout();
-//
-//        assertEquals(homePage.getCurrentUrl(), Locators.popularSellUrl);
-//        System.out.println("Successfully visit popularSell" );
-//    }
+    @Test(priority = 5)
+    public void shoppingList() throws InterruptedException{
+    	commonMethods.clickOnButton(Locators.shoppingListBtn);
+    	timeout.timeout();
+    	Thread.sleep(2000);
+    	commonMethods.setText(Locators.enterBtn, Locators.shoppingText1);
+    	timeout.timeout();
+    	commonMethods.setText(Locators.enterBtn, Locators.shoppingText2);
+    	timeout.timeout();
+    	commonMethods.setText(Locators.enterBtn, Locators.shoppingText3);
+    	timeout.timeout();
+    	commonMethods.setText(Locators.enterBtn, Locators.shoppingText4);
+    	timeout.timeout();
+        System.out.println("Successfully all the items appended on the list");
+    }
 
     @AfterClass
     public void tearDown() {
